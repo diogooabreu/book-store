e11f32fd5626358a6d9c2eeccb1b3652f90aed5c
Diogo N. Abreu <134980133+diogooabreu@users.noreply.github.com>
2026-06-15 17:40:18 -0300

    build: gera modulos de dominio via scaffolding para a api (#11)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
+import { AuthorsModule } from './authors/authors.module';
+import { LoansModule } from './loans/loans.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, BooksModule, AuthorsModule, LoansModule],
   controllers: [AppController],
   providers: [AppService],
 })
